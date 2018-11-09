0983cc4fdd9b178199e083bcf6ef139951ac6510
Anton Andrésen <anton_real_madrid@icloud.com>
2018-11-09 11:39:40 +0100

    added shooting

---
+++ thing/Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public int damage = 1;
+
+    public int speed = 10;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+}
--- thing/Assets/Player.cs
+++ thing/Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,11 @@ public class Player : MonoBehaviour {
     Rigidbody2D rb;
 
     public float speed;
-
     public float jumpForce;
-
     public int numberOfJumps = 2;
+    public bool isRight;
+
+    public GameObject bulletPrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -22,10 +24,27 @@ public class Player : MonoBehaviour {
 
         Move();
         Jump();
+        Shoot();
 	}
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if(isRight)
+                Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            else
+                Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(-transform.forward, Vector3.up));
+        }
+    }
 
     void Move(){
         float dir = Input.GetAxisRaw("Horizontal");
+
+        if (dir < 0)
+            isRight = false;
+        else if (dir > 0)
+            isRight = true;
 
         rb.velocity = new Vector2(dir * speed * Time.deltaTime, rb.velocity.y);
     }
