0c8be88c63a79f2dcf4bcca66c545871fe3d9254
Anton1337 <anton_real_madrid@icloud.com>
2018-11-15 23:57:57 +0100

    Small Adjustments.

--- thing/Assets/Scripts/Camera/Follow.cs
+++ thing/Assets/Scripts/Camera/Follow.cs
@@ -1,15 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Follow : MonoBehaviour {
 
     [SerializeField]
     private Transform following;
 
+    [SerializeField]
+    private RectTransform UIPanel;
+
+    private float playerCenterOffset;
+
+    private void Awake()
+    {
+        playerCenterOffset = UIPanel.rect.width / 2;
+        Debug.Log(playerCenterOffset);
+    }
+
     private void Update()
     {
-        Vector3 newPos = new Vector3(following.position.x, following.position.y, transform.position.z);
+        //TODO: - Fix centering of player.
+        Vector3 newPos = new Vector3(following.position.x /*+(playerCenterOffset / 70)*/, 
+                                     following.position.y, transform.position.z);
         transform.position = newPos;
     }
 }
