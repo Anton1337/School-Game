869e9f0c8abfbaba9711f7bd1a892a2a53f45af2
Anton Andrésen <anton_real_madrid@icloud.com>
2018-11-20 19:55:33 +0100

    Changes to stats.

--- thing/Assets/Scripts/InventoryAndEquipsStuff/EquippableItem.cs
+++ thing/Assets/Scripts/InventoryAndEquipsStuff/EquippableItem.cs
@@ -16,10 +16,5 @@ public class EquippableItem : Item {
     public int AttackSpeedBonus;
     public int DefenseBonus;
     [Space]
-    public float DamagePercentBonus;
-    public float SpeedPercentBonus;
-    public float AttackSpeedPercentBonus;
-    public float DefencePercentBonus;
-    [Space]
     public EquipmentType EquipmentType;
 }
